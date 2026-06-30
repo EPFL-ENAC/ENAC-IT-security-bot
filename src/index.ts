0fcb1e53f8fcd2d2beaf515fde0972f758e55f37
Quentin Eschmann <qeschmann@icloud.com>
2026-06-30 15:06:18 +0200

    feat(bot): bot answering to messages on pr and issues

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,23 @@
 import { Probot } from "probot";
 
 export default (app: Probot) => {
-  app.on("issues.opened", async (context) => {
-    const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
-    });
-    await context.octokit.rest.issues.createComment(issueComment);
+  app.on(["issue_comment.created", "issue_comment.edited", "discussion_comment"], async (context) => {
+    context.log.info(context);
+    if(context.payload.comment.body.startsWith("/enac") && context.isBot === false) {
+      switch(context.payload.comment.body.split(" ")[1]) {
+        case "scan":
+          const issueComment = context.issue({
+            body: "This will be a scan somedays...",
+          });
+          await context.octokit.rest.issues.createComment(issueComment);
+          break;
+        case "help":
+          const issueCommentHelp = context.issue({
+            body: "No worries, I can help you!\n\nprefix: /enac\n\nCommands:\n- help: Display this help message.\n- scan: Scan the repository for security issues.",
+          });
+          await context.octokit.rest.issues.createComment(issueCommentHelp);
+          break;
+      }
+    }
   });
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
 };
